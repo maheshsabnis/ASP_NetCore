622728001bf7caa78ae48bd92f557ab495143a14
maheshsabnis <sabnis_m@hotmail.com>
2019-03-19 16:05:51 +0530

    Add files via upload

---
+++ CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core_WebApp.Models;
+using Core_WebApp.Repositories;
+using Core_WebApp.CustomSessionProvider;
+namespace Core_WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        IRepository<Category, int> catRepository;
+
+        public CategoryController(IRepository<Category, int> catRepository)
+        {
+            
+            this.catRepository = catRepository;
+        }
+        public IActionResult Index()
+        {
+            var cats = catRepository.Get();
+            return View(cats);
+        }
+        // default HttpGet
+        public IActionResult Create()
+        {
+            return View(new Category());
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (category.BasePrice < 0)
+                        throw new Exception("Base Price Cannot be -Ve");
+                    catRepository.Create(category);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(category);
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Error",new ErrorViewModel() {
+                     ControllerName=this.RouteData.Values["controller"].ToString(),
+                     ActionName= this.RouteData.Values["action"].ToString(),
+                     Exception = ex
+                });
+            }
+        }
+        public IActionResult Edit(int id)
+        {
+            if (HttpContext.Session.Keys.Count() > 0)
+            {
+                var cat = HttpContext.Session.Get<Category>("CatData");
+                HttpContext.Session.Remove("CatData");
+                return View(cat);
+            }
+            else
+            {
+                var cat = catRepository.Get(id);
+                return View(cat);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.BasePrice < 0)
+                {
+                    HttpContext.Session.Set<Category>("CatData", category);
+                    throw new Exception("Base Price cannot be -Ve");
+                }
+                   
+                catRepository.Update(id, category);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        public IActionResult Details(int id)
+        {
+         //  TempData["CatRowId"] = id;
+             var data = new byte[] { (byte)id };
+          HttpContext.Session.Set("CatRowId",data);
+        
+            return RedirectToAction("Index","Product");
+        }
+    }
+}
---
+++ CustomSessionProvider/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_WebApp.CustomSessionProvider
+{
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            // Serialoize the CLR Object in the JSON format
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            // read the JSON string
+            var value = session.GetString(key);
+            // DeSerialize the JSON string into CLR Object
+            return value == null ? default(T) :
+                JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
